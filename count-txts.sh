53b53cfd007113c72815fd2a14fbf6665eecfdd3
Jessyjy7 <jiy018@ucsd.edu>
2023-04-26 16:42:21 -0700

    lab 4 changes

---
+++ count-txts.sh
@@ -0,0 +1,3 @@
+find non-fiction > find-results.txt
+grep ".txt" find-results.txt > grep-results.txt
+wc grep-results.txt
---
+++ start.sh
@@ -0,0 +1,3 @@
+
+javac Server.java DocSearchServer.java TestDocSearch.java
+java DocSearchServer $1 $2
